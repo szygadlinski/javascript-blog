1c0f8b61e2c26b42a412bffaacf16e09afec9a1c
Szymon Zygadliński <szygadlinski@gmail.com>
2020-11-14 04:47:07 +0100

    Add js for displaying articles

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@
 }) */
 
 const titleClickHandler = function(event){
+	event.preventDefault();
 	const clickedElement = this;
 	console.log('Link was clicked!');
 	console.log(event);
@@ -15,7 +16,6 @@ const titleClickHandler = function(event){
 	/* remove class 'active' from all article links */
 
 	const activeLinks = document.querySelectorAll('.titles a.active');
-
 	for (let activeLink of activeLinks){
 		activeLink.classList.remove('active');
 	}
@@ -23,22 +23,29 @@ const titleClickHandler = function(event){
 	/* add class 'active' to the clicked link */
 
 	console.log('clicked element:', clickedElement);
-
 	clickedElement.classList.add('active');
 
 	/* remove class 'active' from all articles */
 
 	const activeArticles = document.querySelectorAll('.posts .active');
-
 	for (let activeArticle of activeArticles){
 		activeArticle.classList.remove('active');
 	}
 
 	/* get 'href' attribute from the clicked link */
 
+	const articleSelector = clickedElement.getAttribute('href');
+	console.log('selected article:', articleSelector);
+
 	/* find the correct article using the selector (value of 'href' attribute) */
 
+	const targetArticle = document.querySelector(articleSelector);
+	console.log('target article:', targetArticle);
+
 	/* add class 'active' to the correct article */
+
+	targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
@@ -46,6 +53,5 @@ const links = document.querySelectorAll('.titles a');
 for (let link of links){
 	link.addEventListener('click', titleClickHandler);
 }
-
 
 }
